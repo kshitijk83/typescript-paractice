0df43b05cd977727ad2370bde20ccff5a3fac575
kshitijk83 <kshitijk83@gmail.com>
2020-04-15 04:43:40 +0530

    initial setup

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number = 5;
+var number2 = 2;
+var result = add(number, number2);
+console.log(result);
+//# sourceMappingURL=app.js.map
