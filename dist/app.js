9b7452aaa6c6babc7239c8f3ac7e0f6da88b45f1
kshitijk83 <kshitijk83@gmail.com>
2020-04-16 01:32:56 +0530

    decorators and generics

--- dist/app.js
+++ dist/app.js
@@ -1,45 +1,24 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(val) {
+    return function (target) {
+        console.log(val);
+        console.log(target);
     };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Department = (function () {
-    function Department(id, name) {
-        this.id = id;
-        this.name = name;
-        this.emp = [];
+}
+var Person = (function () {
+    function Person() {
+        this.name = "max";
+        console.log("creating person...");
     }
-    Department.prototype.describe = function () {
-        console.log("Department: " + this.name);
-    };
-    Department.prototype.addEmp = function (emp) {
-        this.emp.push(emp);
-    };
-    Department.prototype.printEmp = function () {
-        console.log(this.emp);
-    };
-    return Department;
+    Person = __decorate([
+        Logger("Contructor-person")
+    ], Person);
+    return Person;
 }());
-var IT = (function (_super) {
-    __extends(IT, _super);
-    function IT(id, reports) {
-        var _this = _super.call(this, id, 'IT') || this;
-        _this.reports = reports;
-        return _this;
-    }
-    IT.prototype.addRep = function (rep) {
-        this.reports.push(rep);
-    };
-    return IT;
-}(Department));
-var x = new IT("sad", ["asdf"]);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,18 @@
-interface Named{
-    readonly name: string;
-
-}
-
-interface Greetable extends Named{
-    greet(phrase: string): void;
-}
-
-// type add=(n1: number, n2: number)=>number
-interface params{
-    n1:number;
-    n2:number;
-}
-function add(config: params):void{
-    
-}
-
-class Person implements Greetable{
-    constructor(public name: string, public id: string){
-
-    }
-
-    greet(phrase: string){
-        // sdfs
-    }
-}
-let x = new Person("asdf", "asd");
-
-interface cat{
-    type: 'cat';
-    run: number;
-}
-
-interface fish{
-    type: 'fish';
-    float: number;
-}
-type Animal = cat|fish;
-function blah(animal:Animal){
-    switch (animal.type) {
-        case 'cat':
-            console.log(animal.run)
-            break;
-        case 'fish':
-            console.log(animal.float)
-        default:
-            break;
+// function merge<T extends object, U extends object>(a: T, b: U){
+//     return Object.assign(a, b)
+// }
+// let x=merge({},{})
+function Logger(val: string){
+    return function(target: Function){
+        console.log(val)
+        console.log(target)
     }
 }
 
-let userInput = document.getElementById("hehe") as HTMLInputElement;
-userInput.value="asdf"
+@Logger("Contructor-person")
+class Person{
+    name="max"
+    constructor(){
+        console.log("creating person...")
+    }
+}
